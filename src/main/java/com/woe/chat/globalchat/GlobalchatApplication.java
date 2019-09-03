6382ee3d2b028be7dc5a732cd385c5de8613a2ce
bariscanakdag <bariscan.akdag155@gmail.com>
2019-09-03 13:54:09 +0300

    init

---
+++ src/main/java/com/woe/chat/globalchat/GlobalchatApplication.java
@@ -0,0 +1,13 @@
+package com.woe.chat.globalchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woe/chat/globalchat/GlobalchatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woe.chat.globalchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlobalchatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
